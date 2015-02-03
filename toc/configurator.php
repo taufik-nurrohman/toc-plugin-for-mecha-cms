f76ae1a284b832a0e8defc1b38dd4dd6fb67a77b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-02-03 19:54:04 +0700

    Make `back:` Prefix Optional

--- toc/configurator.php
+++ toc/configurator.php
@@ -17,6 +17,14 @@
     <span class="grid span-4"><input name="id_suffix" type="text" value="<?php echo Guardian::wayback('id_suffix', $toc_config['id_suffix']); ?>" placeholder="<?php echo $speak->plugin_toc_placeholder_suffix; ?>"></span>
   </label>
   <label class="grid-group">
+    <span class="grid span-2 form-label"><?php echo $speak->plugin_toc_title_back_prefix; ?></span>
+    <span class="grid span-4"><input name="id_back_prefix" type="text" value="<?php echo Guardian::wayback('id_back_prefix', $toc_config['id_back_prefix']); ?>" placeholder="<?php echo $speak->plugin_toc_placeholder_back_prefix; ?>"></span>
+  </label>
+  <label class="grid-group">
+    <span class="grid span-2 form-label"><?php echo $speak->plugin_toc_title_back_suffix; ?></span>
+    <span class="grid span-4"><input name="id_back_suffix" type="text" value="<?php echo Guardian::wayback('id_back_suffix', $toc_config['id_back_suffix']); ?>" placeholder="<?php echo $speak->plugin_toc_placeholder_back_suffix; ?>"></span>
+  </label>
+  <label class="grid-group">
     <span class="grid span-2 form-label"><?php echo $speak->plugin_toc_title_css; ?></span>
     <span class="grid span-4"><textarea name="css" class="textarea-block code"><?php echo Text::parse(Guardian::wayback('css', $toc_css))->to_encoded_html; ?></textarea></span>
   </label>
--- toc/launch.php
+++ toc/launch.php
@@ -5,12 +5,14 @@ $toc_config = File::open(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 
 
 Config::set('toc_id', 1);
 
-function page_TOC($content) {
+function do_TOC($content) {
     global $toc_config;
     $config = Config::get();
     $speak = Config::speak();
     $prefix = $toc_config['id_prefix'];
     $suffix = $toc_config['id_suffix'];
+    $prefix_b = $toc_config['id_back_prefix'];
+    $suffix_b = $toc_config['id_back_suffix'];
     $regex = '#<h([1-6]) *(.*?)>(.*?)<\/h\1>#';
     $repeat = 0;
     $depth = 0;
@@ -18,7 +20,7 @@ function page_TOC($content) {
     if(preg_match_all($regex, $content, $matches)) {
         for($i = 0, $count = count($matches[0]); $i < $count; ++$i) {
             $level = (int) $matches[1][$i];
-            if( ! preg_match('# ?class="(.*?) ?not-toc-stage ?(.*?)"#', $matches[2][$i])) {
+            if( ! preg_match('# +class="(.*?) *not-toc-stage *(.*?)"#', $matches[2][$i])) {
                 if($depth < $level) {
                     $toc .= '<ol>';
                     $depth = $level;
@@ -31,17 +33,17 @@ function page_TOC($content) {
                     $depth = $level;
                     $repeat--;
                 }
-                $title = preg_replace('#<a .*?>|<\/a>#', "", $matches[3][$i]);
-                if(preg_match('#id="(.*?)"#', $matches[2][$i], $id)) {
-                    $toc .= '<li id="back:' . $config->toc_id . '-' . ($i + 1) . '"><a href="#' . $id[1] . '">' . trim($title) . '</a>';
+                $title = preg_replace('#<a(>| .*?>)|<\/a>#', "", $matches[3][$i]);
+                if(preg_match('#(^| )id="(.*?)"#', $matches[2][$i], $ma)) {
+                    $toc .= '<li id="' . $prefix_b . $config->toc_id . '-' . ($i + 1) . $suffix_b . '"><a href="#' . $ma[2] . '">' . trim($title) . '</a>';
                 } else {
-                    $toc .= '<li id="back:' . $config->toc_id . '-' . ($i + 1) . '"><a href="#' . $prefix . Text::parse($title)->to_slug . $suffix . '">' . trim($title) . '</a>';
+                    $toc .= '<li id="' . $prefix_b . $config->toc_id . '-' . ($i + 1) . $suffix_b . '"><a href="#' . $prefix . Text::parse($title)->to_slug . $suffix . '">' . trim($title) . '</a>';
                 }
                 $toc .= ' <span class="marker">&#9666;</span>';
             }
         }
         $counter = 0;
-        $content = preg_replace_callback($regex, function($matches) use($config, $speak, $toc_config, $prefix, $suffix, $repeat, &$counter) {
+        $content = preg_replace_callback($regex, function($matches) use($config, $speak, $toc_config, $prefix, $suffix, $prefix_b, $suffix_b, $repeat, &$counter) {
             $counter++;
             if(strpos($matches[2], 'class="') !== false) {
                 $attrs = ' ' . trim(str_replace('class="', 'class="toc-stage ', $matches[2]));
@@ -52,26 +54,26 @@ function page_TOC($content) {
                 $attrs .= ' id="' . $prefix . Text::parse($matches[3])->to_slug . $suffix . '"';
             }
             if($toc_config['add_toc']) {
-                $anchor = '<a class="toc-back" href="#back:' . $config->toc_id . '-' . $counter . '"' . ( ! empty($toc_config['toc_back_title']) ? ' title="' . $toc_config['toc_back_title'] . '"' : "") . '>&#9652;</a>';
+                $anchor = '<a class="toc-back" href="#' . $prefix_b . $config->toc_id . '-' . $counter . $suffix_b . '"' . ( ! empty($toc_config['toc_back_title']) ? ' title="' . $toc_config['toc_back_title'] . '"' : "") . '>&#9652;</a>';
             } else {
                 if(strpos($matches[2], 'id="') === false) {
-                    $anchor = '<a class="toc-permalink" href="#' . $prefix . Text::parse($matches[3])->to_slug . $suffix . '" title="' . $speak->permalink . '">&#182;</a>';
+                    $anchor = '<a class="toc-permalink" href="#' . $prefix . Text::parse($matches[3])->to_slug . $suffix . '" title="' . $speak->permalink . '">&#167;</a>';
                 } else {
-                    preg_match('#id="(.*?)"#i', $matches[2], $m);
-                    $anchor = '<a class="toc-permalink" href="#' . $m[1] . '" title="' . $speak->permalink . '">&#182;</a>';
+                    preg_match('#(^| )id="(.*?)"#i', $matches[2], $ma);
+                    $anchor = '<a class="toc-permalink" href="#' . $ma[2] . '" title="' . $speak->permalink . '">&#167;</a>';
                 }
             }
-            return '<h' . $matches[1] . str_replace('  id="', ' id="', $attrs) . '>' . trim($matches[3]) . ( ! preg_match('# ?class="(.*?) ?not-toc-stage ?(.*?)"#', $matches[2]) ? ' ' . $anchor : "") . '</h' . $matches[1] . '>';
+            return '<h' . $matches[1] . $attrs . '>' . trim($matches[3]) . ( ! preg_match('# ?class="(.*?) *not-toc-stage *(.*?)"#', $matches[2]) ? ' ' . $anchor : "") . '</h' . $matches[1] . '>';
         }, $content);
-        return ($toc_config['add_toc'] && ! empty($toc) ? '<div class="toc-block" id="toc-block-' . $config->toc_id . '">' . ( ! empty($toc_config['toc_title']) ? '<h3 class="toc-header">' . $toc_config['toc_title'] . '</h3>' : "") . $toc . str_repeat('</li></ol>', $repeat) . '</div>' : "") . $content;
+        return ($toc_config['add_toc'] && ! empty($toc) ? '<div class="toc-block" id="toc-block:' . $config->toc_id . '">' . ( ! empty($toc_config['toc_title']) ? '<h3 class="toc-header">' . $toc_config['toc_title'] . '</h3>' : "") . $toc . str_repeat('</li></ol>', $repeat) . '</div>' : "") . $content;
     }
     Config::set('toc_id', $config->toc_id + 1);
     return $content;
 }
 
 // Register the filters
-Filter::add('article:content', 'page_TOC');
-Filter::add('page:content', 'page_TOC');
+Filter::add('article:content', 'do_TOC');
+Filter::add('page:content', 'do_TOC');
 
 // Add CSS for table of content
 Weapon::add('shell_after', function() {
@@ -90,11 +92,11 @@ Route::accept($config->manager->slug . '/plugin/' . basename(__DIR__) . '/update
     }
     if($request = Request::post()) {
         Guardian::checkToken($request['token']);
-        File::write($request['css'])->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'shell' . DS . 'toc.css');
         unset($request['token']); // Remove token from request array
+        File::write($request['css'])->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'shell' . DS . 'toc.css');
         unset($request['css']); // Remove CSS from request array
         $request['add_toc'] = isset($request['add_toc']) ? true : false;
-        File::serialize($request)->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 'config.txt');
+        File::serialize($request)->saveTo(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 'config.txt', 0600);
         Notify::success(Config::speak('notify_success_updated', array($speak->plugin)));
         Guardian::kick(dirname($config->url_current));
     }
