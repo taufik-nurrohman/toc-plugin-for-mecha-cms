a42d822ad0b2b90facea3682157c4dc7875afcc5
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-02-23 22:04:41 +0700

    First Fix for TOC Depth Bugs

--- toc/launch.php
+++ toc/launch.php
@@ -13,61 +13,67 @@ function do_TOC($content) {
     $suffix = $toc_config['id_suffix'];
     $prefix_b = $toc_config['id_back_prefix'];
     $suffix_b = $toc_config['id_back_suffix'];
-    $regex = '#<h([1-6])(>| +.*?>)(.*?)<\/h\1>#';
+    $regex = '#<h([1-6])(>| +.*?>)(.*?)<\/h\1>#i';
+    $regex_exc = '#(?:^| )class=".*? *not-toc-stage *.*?"#i';
     $repeat = 0;
     $depth = 0;
     $toc = "";
     if(preg_match_all($regex, $content, $matches)) {
-        for($i = 0, $count = count($matches[0]); $i < $count; ++$i) {
-            $level = (int) $matches[1][$i];
-            $matches[2][$i] = rtrim($matches[2][$i], '>');
-            if( ! preg_match('#(^| )class="(.*?) *not-toc-stage *(.*?)"#', $matches[2][$i])) {
-                if($depth < $level) {
-                    $toc .= '<ol>';
-                    $depth = $level;
-                    $repeat++;
-                } else {
-                    $toc .= '</li>';
+        if($toc_config['add_toc']) {
+            for($i = 0, $count = count($matches[0]); $i < $count; ++$i) {
+                $level = (int) $matches[1][$i];
+                $matches[2][$i] = rtrim($matches[2][$i], '>');
+                if( ! preg_match($regex_exc, $matches[2][$i])) {
+                    if($depth < $level) {
+                        $toc .= str_repeat('<ol>', $i > 0 ? $level - $depth : 1);
+                        $repeat++;
+                        $depth = $level;
+                    } else {
+                        $toc .= '</li>';
+                    }
+                    if($depth > $level) {
+                        $toc .= str_repeat('</ol></li>', $i > 0 ? $depth - $level : 1);
+                        $repeat = $depth - $level;
+                        $depth = $level;
+                    }
+                    $title = preg_replace('#<a(>| .*?>)|<\/a>#', "", $matches[3][$i]);
+                    if(preg_match('#(?:^| )id="(.*?)"#', $matches[2][$i], $id)) {
+                        $toc .= '<li id="' . $prefix_b . $config->toc_id . '-' . ($i + 1) . $suffix_b . '"><a href="#' . $id[1] . '">' . trim($title) . '</a>';
+                    } else {
+                        $toc .= '<li id="' . $prefix_b . $config->toc_id . '-' . ($i + 1) . $suffix_b . '"><a href="#' . $prefix . Text::parse($title)->to_slug . $suffix . '">' . trim($title) . '</a>';
+                    }
+                    $toc .= ' <span class="marker">&#9666;</span>';
                 }
-                if($depth > $level) {
-                    $toc .= '</ol></li>';
-                    $depth = $level;
-                    $repeat--;
-                }
-                $title = preg_replace('#<a(>| .*?>)|<\/a>#', "", $matches[3][$i]);
-                if(preg_match('#(^| )id="(.*?)"#', $matches[2][$i], $ma)) {
-                    $toc .= '<li id="' . $prefix_b . $config->toc_id . '-' . ($i + 1) . $suffix_b . '"><a href="#' . $ma[2] . '">' . trim($title) . '</a>';
-                } else {
-                    $toc .= '<li id="' . $prefix_b . $config->toc_id . '-' . ($i + 1) . $suffix_b . '"><a href="#' . $prefix . Text::parse($title)->to_slug . $suffix . '">' . trim($title) . '</a>';
-                }
-                $toc .= ' <span class="marker">&#9666;</span>';
             }
         }
         $counter = 0;
-        $content = preg_replace_callback($regex, function($matches) use($config, $speak, $toc_config, $prefix, $suffix, $prefix_b, $suffix_b, $repeat, &$counter) {
-            $counter++;
-            $matches[2] = rtrim($matches[2], '>');
-            if(strpos($matches[2], 'class="') !== false) {
-                $attrs = ' ' . trim(str_replace('class="', 'class="toc-stage ', $matches[2]));
-            } else {
-                $attrs = ' class="toc-stage" ' . trim($matches[2]);
-            }
-            if(strpos($matches[2], 'id="') === false) {
-                $attrs .= ' id="' . $prefix . Text::parse($matches[3])->to_slug . $suffix . '"';
-            }
-            if($toc_config['add_toc']) {
-                $anchor = '<a class="toc-back" href="#' . $prefix_b . $config->toc_id . '-' . $counter . $suffix_b . '"' . ( ! empty($toc_config['toc_back_title']) ? ' title="' . $toc_config['toc_back_title'] . '"' : "") . '>&#9652;</a>';
-            } else {
-                if(strpos($matches[2], 'id="') === false) {
-                    $anchor = '<a class="toc-permalink" href="#' . $prefix . Text::parse($matches[3])->to_slug . $suffix . '" title="' . $speak->permalink . '">&#167;</a>';
+        $content = preg_replace_callback($regex, function($matches) use($config, $speak, $toc_config, $prefix, $suffix, $prefix_b, $suffix_b, $regex_exc, &$counter) {
+            if( ! preg_match($regex_exc, $matches[2])) {
+                $counter++;
+                $matches[2] = rtrim($matches[2], '>');
+                if(strpos($matches[2], 'class="') !== false) {
+                    $attrs = ' ' . trim(str_replace('class="', 'class="toc-stage ', $matches[2]));
                 } else {
-                    preg_match('#(^| )id="(.*?)"#i', $matches[2], $ma);
-                    $anchor = '<a class="toc-permalink" href="#' . $ma[2] . '" title="' . $speak->permalink . '">&#167;</a>';
+                    $attrs = ' class="toc-stage" ' . trim($matches[2]);
                 }
+                if(strpos($matches[2], 'id="') === false) {
+                    $attrs .= ' id="' . $prefix . Text::parse($matches[3])->to_slug . $suffix . '"';
+                }
+                if($toc_config['add_toc']) {
+                    $anchor = '<a class="toc-back" href="#' . $prefix_b . $config->toc_id . '-' . $counter . $suffix_b . '"' . ( ! empty($toc_config['toc_back_title']) ? ' title="' . $toc_config['toc_back_title'] . '"' : "") . '>&#9652;</a>';
+                } else {
+                    if(strpos($matches[2], 'id="') === false) {
+                        $anchor = '<a class="toc-permalink" href="#' . $prefix . Text::parse($matches[3])->to_slug . $suffix . '" title="' . $speak->permalink . '">&#167;</a>';
+                    } else {
+                        preg_match('#(?:^| )id="(.*?)"#i', $matches[2], $id);
+                        $anchor = '<a class="toc-permalink" href="#' . $id[1] . '" title="' . $speak->permalink . '">&#167;</a>';
+                    }
+                }
+                return '<h' . $matches[1] . str_replace('  id="', ' id="', $attrs) . '>' . trim($matches[3]) . $anchor . '</h' . $matches[1] . '>';
             }
-            return '<h' . $matches[1] . $attrs . '>' . trim($matches[3]) . ( ! preg_match('#(^| )class="(.*?) *not-toc-stage *(.*?)"#', $matches[2]) ? ' ' . $anchor : "") . '</h' . $matches[1] . '>';
+            return $matches[0];
         }, $content);
-        return ($toc_config['add_toc'] && ! empty($toc) ? '<div class="toc-block" id="toc-block:' . $config->toc_id . '">' . ( ! empty($toc_config['toc_title']) ? '<h3 class="toc-header">' . $toc_config['toc_title'] . '</h3>' : "") . $toc . str_repeat('</li></ol>', $repeat) . '</div>' : "") . $content;
+        return ($toc_config['add_toc'] && ! empty($toc) ? '<div class="toc-block" id="toc-block:' . $config->toc_id . '">' . ( ! empty($toc_config['toc_title']) ? '<h3 class="toc-header">' . $toc_config['toc_title'] . '</h3>' : "") . '<div class="toc-content">' . $toc . str_repeat('</li></ol>', $repeat) . '</div></div>' : "") . $content;
     }
     Config::set('toc_id', $config->toc_id + 1);
     return $content;
