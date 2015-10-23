14fdaf89708dfa38b5101ebcab3ff2b71a19cc3b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-10-23 19:03:30 +0700

    Update Scope

--- toc/workers/fields.php
+++ toc/workers/fields.php
@@ -3,6 +3,7 @@
 return array(
     'disable_toc' => array(
         'title' => $speak->plugin_toc_title_toc_disable,
-        'type' => 'b'
+        'type' => 'boolean',
+        'scope' => 'article,page'
     )
 );
